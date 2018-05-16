6be72cebbe1b136b7f299168cc1f63142627a382
wenyang <coder@1024do.com>
2018-05-16 14:14:42 +0800

    Automatically register url handlers

---
+++ RequestHandler/aiohttp.py
@@ -0,0 +1,47 @@
+# RequestHandler目的是从url函数中分析其需要接受的参数
+# 调用url函数将结果转换为web.Response对象(符合aiohttp框架的要求)
+
+class RequestHandler(object):
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+        ...
+
+    @asyncio.coroutine
+    def __call__(self, request):
+        kw = ... # 获取参数
+        r = yield  from self._func(**kw)
+        return r
+
+# 注册单个url处理函数
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+
+# 自动注册handler模块所有符合条件的函数
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == -1:
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
+
+
+
